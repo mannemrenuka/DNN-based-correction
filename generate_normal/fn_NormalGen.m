b80299ce3d4c41dede3071a5d5939a824ee79a69
mannemrenuka <43288347+mannemrenuka@users.noreply.github.com>
2020-05-24 15:05:03 +0530

    Add files via upload

---
+++ generate_normal/fn_NormalGen.m
@@ -0,0 +1,15 @@
+function R= fn_NormalGen(p1,p2,p3,n,pxl)
+m=(p3(2)-p1(2))/(p3(1)-p1(1));
+mn=-1/m;
+if (m~=0)
+    deltax=pxl/(n*sqrt(1+mn^2));
+    x=p2(1)+deltax*(-n:n);
+    y=mn*x+(p2(2)-mn*p2(1));
+    R=[x' y'];
+else
+    x=p2(1)*ones(1,2*n +1);
+    y=p2(2)+ (pxl/n)*(-n:n);
+    R=[x' y'];
+end
+end
+
